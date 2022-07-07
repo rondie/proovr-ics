4401fe5152e07d4415eaab2a5cdb61c1f72d3250
rondie <rondie@rocinante.ron>
2022-07-07 20:54:12 +0200

    clean up for flake8

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,10 @@ import requests
 app = Flask(__name__)
 
 
-headers = {"Accept": "application/json, text/plain, */*", "Application-Agent": "Proovr"}
+headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Application-Agent": "Proovr"
+        }
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -35,32 +38,61 @@ def getconfcode(emailurl):
     if request.method == "POST":
         confcode = request.form['E-mail Confirmation Code']
         email = urllib.parse.unquote(emailurl)
-        emailurl = urllib.parse.quote(email.encode("utf8"))
+        emailurl = \
+            urllib.parse.quote(email.encode("utf8"))
         if confcode:
             state = uuid.uuid5(uuid.NAMESPACE_URL, email)
-            emailCredentialData = {"Email": email, "State": str(state), "Token": confcode}
-            emailCredentialPage = requests.post('https://api.proovr.com/v0.1/credentials/email/verify', json=emailCredentialData)
+            emailCredentialData = {
+                "Email": email,
+                "State": str(state),
+                "Token": confcode
+                }
+            emailCredentialPage = requests.post(
+                'https://api.proovr.com/v0.1/credentials/email/verify',
+                json=emailCredentialData
+                )
             if emailCredentialPage.status_code != 200:
-                return render_template("page.html", email=email, emailurl=emailurl, error=emailCredentialPage.json())
+                return render_template(
+                    "page.html",
+                    email=email,
+                    emailurl=emailurl,
+                    error=emailCredentialPage.json()
+                    )
             else:
                 emailCredentialPageJson = json.loads(emailCredentialPage.text)
                 emailCredential = emailCredentialPageJson['emailCredential']
                 getBearerData = {"Credentials": {"Email": emailCredential}}
-                getBearerPage = requests.post('https://proovr.proxyclick.com/employee/view-booking', headers=headers, json=getBearerData)
+                getBearerPage = requests.post(
+                    'https://proovr.proxyclick.com/employee/view-booking',
+                    headers=headers,
+                    json=getBearerData
+                    )
                 redirectUrlJson = json.loads(getBearerPage.text)
-                redirectUrlMatch = re.search('jwt=([a-z].*)&', str(redirectUrlJson))
+                redirectUrlMatch = re.search(
+                    'jwt=([a-z].*)&',
+                    str(redirectUrlJson)
+                    )
                 bearer = redirectUrlMatch.group(1)
                 return redirect(emailurl + '/' + bearer)
         else:
             mailConfCode(email)
-            return render_template("page.html", email=email, emailurl=emailurl)
+            return render_template(
+                "page.html",
+                email=email,
+                emailurl=emailurl)
 
 
 @app.route('/<emailurl>/<bearer>')
 def icsPage(emailurl, bearer):
     email = urllib.parse.unquote(emailurl)
     bookedData = getBookedData(email, bearer)
-    return render_template("page.html", email=email, emailurl=emailurl, bearer=bearer, bookedData=bookedData)
+    return render_template(
+        "page.html",
+        email=email,
+        emailurl=emailurl,
+        bearer=bearer,
+        bookedData=bookedData
+        )
 
 
 @app.route('/<emailurl>/<bearer>/proovr.ics')
